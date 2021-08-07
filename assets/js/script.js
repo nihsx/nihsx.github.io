e57c1538d38bb52489314ffb06acdb53a0c5a4a4
xshin404 <xshin3373@gmail.com>
2021-08-07 22:11:37 +0700

    myTermux v.0.5.0 dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
-hljs.initHighlightingOnLoad();
+$.ajax({
+  url: 'https://api.github.com/repos/xshin404/myTermux',
+  success: results => {
+    let api = '';
+    api += dom(results);
+    $('.api').html(api);
+  },
+  error: (e) => {
+    console.log(e.responsText);
+  }
+});
 
-hljs.initLineNumbersOnLoad();
+function dom(data) {
+  return `
+  <ul>
+    <li><i class="fas fa-star text-warning me-2"></i>${data.stargazers_count}</li>
+    <li><i class="fas fa-code-branch me-3"></i>${data.forks_count}</li>
+  </ul>
+  `
+}
